0b65bb8382752cb1aa4a0aa3fd2edb2e7feec57d
Andrew Menadue <menadue@gmail.com>
2020-12-13 20:46:04 +0000

    Added dump to romdata file tool

---
+++ traces/convert_romdump_to_romdata.tcl
@@ -0,0 +1,25 @@
+#!/usr/bin/tclsh
+#
+
+set fn [lindex $argv 0]
+set f [open $fn]
+set txt [read $f]
+close $f
+
+
+set f [open $fn.romdata w]
+
+
+
+foreach line [split $txt "\n"] {
+    foreach byte [split $line " "] {
+        if { [string length $byte] != 0 } {
+            puts $f [format "%02X" [expr 0x$byte]]
+        }
+    }
+}
+
+
+
+
+close $f
